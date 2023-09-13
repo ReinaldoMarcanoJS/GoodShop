7158cb3597036ae318b5e8d22f36000e7a815b3a
Reinaldo Marcano <reynaldomarcano4@gmail.com>
2023-09-13 12:53:56 -0400

    GoodShop

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,12 +2,6 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: "https://goodshopbackend-production.up.railway.app/api",
-    withCredentials: true,
-    headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-    },
 })
 
 export default instance
