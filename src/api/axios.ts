63bd7a22bee3aa51885e9a254fde89e001ba1a16
Reinaldo Marcano <reynaldomarcano4@gmail.com>
2023-09-13 11:36:14 -0400

    commit

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-
 const instance = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: "https://goodshopbackend-production.up.railway.app/api",
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
