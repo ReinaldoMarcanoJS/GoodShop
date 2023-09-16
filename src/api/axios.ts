64e6ddcd0a7bad1cf548a9e0f1039f545a257106
Reinaldo Marcano <reynaldomarcano4@gmail.com>
2023-09-16 15:18:43 -0400

    logout

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,9 +3,10 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://goodshopbackend-production.up.railway.app/api",
   // baseURL: "http://localhost:3001/api",
-
+  
   headers: {
     "Content-Type": "application/json",
+    "same-site": "none"
     Accept: "application/json",
   },
   withCredentials: true
