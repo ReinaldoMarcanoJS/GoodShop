c1df7afebd007b7ad093766e42a4d64ea3515ef0
Reinaldo Marcano <reynaldomarcano4@gmail.com>
2023-09-26 12:36:21 -0400

    Conected with backend to render products

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://goodshopbackend.up.railway.app/api",
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://goodshopbackend.up.railway.app/api",
+  // baseURL: "http://localhost:3001/api",
   
   headers: {
     "Content-Type": "multipart/form-data",
